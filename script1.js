2c91279014fbddb9d99a07b334e280eae4e642ba
vidya <vidyakambar18@gmail.com>
2023-01-18 22:43:33 +0530

    js4

---
+++ script1.js
@@ -0,0 +1,49 @@
+function Display()
+{
+	console.log("heelo");
+}
+Display();
+
+var x=21;
+var girl = function()
+{
+	console.log(x);
+	var x = 20;
+};
+girl();
+
+
+var x = 21;
+girl ();
+console.log(x)
+function girl() {
+    console.log(x);
+    var x = 20;
+};
+
+
+
+
+
+var x = 21;
+a();
+b();
+
+  function a() {
+    
+   x = 20;
+  console.log(x);
+//};
+ //function b() {
+    
+   // x = 40;
+   //console.log(x);
+//};
+
+
+
+function ABC(a,b){
+	let sum = a + b;
+console.log(" sum of " + a + "," + b + " is " + sum);
+}
+
